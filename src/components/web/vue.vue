7e4bf3efd44c43d97ce342caee9dea185316cc49
wanghuatong <wanghuatong1988@gmail.com>
2018-10-18 11:49:53 +0800

    es6文件类型转换
    Signed-off-by: wanghuatong <wanghuatong1988@gmail.com>

--- src/components/web/vue.vue
+++ src/components/web/vue.vue
@@ -221,6 +221,107 @@
 	</div>
 </template>
 <script>
+
+	// const Observer = function(data) {
+	// 	for (let key in data) {
+	// 		defineReactive(data, key);
+	// 	}
+	// }
+
+	// const defineReactive = function(obj, key) {
+	// 	const dep = new Dep();
+	// 	let val = obj[key];
+	// 	Object.defineProperty(obj, key, {
+	// 		enumerable: true,
+	// 		configurable: true,
+	// 		get() {
+	// 			dep.depend();
+	// 			return val;
+	// 		},
+	// 		set(newVal) {
+	// 			if(newVal === val) {
+	// 				return;
+	// 			}
+	// 			val = newVal;
+	// 			dep.notify();
+	// 		}
+	// 	})
+	// }
+
+	// const observe = function(data) {
+	// 	return new Observer(data);
+	// }
+
+	// const Vue = function(options) {
+	// 	const self = this;
+
+	// 	if(options && typeof options.data === 'function') {
+	// 		this._data = options.data.apply(this);
+	// 	}
+
+	// 	this.mount = function() {
+	// 		new Watcher(self, self.render);
+	// 	}
+
+	// 	this.render = function() {
+	// 		with(self) {
+	// 			_data.text;
+	// 		}
+	// 	}
+
+	// 	observe(this._data);
+	// }
+
+	// const Watcher = function(vm, fn) {
+	// 	const self = this;
+	// 	this.vm = vm;
+	// 	Dep.target = this;
+
+	// 	this.addDep = function(dep) {
+	// 		dep.addSub(self);
+	// 	}
+
+	// 	this.update = function() {
+	// 		fn();
+	// 	}
+
+	// 	this.value = fn();
+
+	// 	Dep.target = null;
+	// }
+
+	// const Dep = function() {
+	// 	const self = this;
+	// 	this.target = null;
+	// 	this.subs = [];
+	// 	this.depend = function() {
+	// 		if(Dep.target) {
+	// 			Dep.target.addDep(self);
+	// 		}
+	// 	}
+
+	// 	this.addSub = function(watcher) {
+	// 		self.subs.push(watcher);
+	// 	}
+
+	// 	this.notify = function() {
+	// 		for(let i = 0; i < self.subs.length; i += 1) {
+	// 			self.subs[i].update();
+	// 		}
+	// 	}
+	// }
+
+	// const vue = new vue({
+	// 	data() {
+	// 		return {
+	// 			text: 'hello world'
+	// 		}
+	// 	}
+	// })
+
+	// vue.mount();
+	// vue._data.text = '123';
+
 </script>
 <style lang="scss" type="text/css">
 ul,li {
